4e43a654221d059eddcdcfa06806a4c8ab074885
Hazem Abouassi <hazem.abouassi@dci.education>
2023-07-19 16:09:25 +0200

    made changes in the comment section

--- client/src/components/sections/Blog.jsx
+++ client/src/components/sections/Blog.jsx
@@ -1,11 +1,3 @@
-import React, { Component } from 'react'
-
-export class Blog extends Component {
-  render() {
-    return (
-      <div>Blog</div>
-    )
-  }
+export default function Blog() {
+  return <div>Blog</div>;
 }
-
-export default Blog
